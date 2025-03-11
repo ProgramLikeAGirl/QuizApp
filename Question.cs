8c924f5e6668ccbdd29619fbf06178c7c7630360
MacWhitehead <69973482+MacWhitehead@users.noreply.github.com>
2025-03-10 19:53:45 -0600

    Add project files.

---
+++ Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    internal class Question
+    {
+        public string QuestionText { get; set; }
+        public string[] Answers { get; set; }
+        public int CorrectAnswerIndex { get; set; }
+
+        public Question()
+        {
+
+        }
+    }
+}
